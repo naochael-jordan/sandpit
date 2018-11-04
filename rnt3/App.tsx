e6ea3ce78a43105d408c44b23155a1a74ad22905
naochael-jordan <naochael.jordan@gmail.com>
2018-11-04 11:16:42 +0900

    build: add ts types

--- rnt3/App.tsx
+++ rnt3/App.tsx
@@ -23,7 +23,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>222Welcome to React Native!</Text>
+        <Text style={styles.welcome}>444Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
