d4add14ff04dbebf0b2901f9193255f1c73e7c56
naochael-jordan <naochael.jordan@gmail.com>
2018-11-04 11:08:42 +0900

    rename file

--- rnt3/App.tsx
+++ rnt3/App.tsx
@@ -9,7 +9,7 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
-console.log(11111);
+console.log(222);
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -23,7 +23,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>222Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
