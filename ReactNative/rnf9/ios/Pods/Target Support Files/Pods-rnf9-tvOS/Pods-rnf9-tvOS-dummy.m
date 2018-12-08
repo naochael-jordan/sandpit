3aae1665f8cfe1f820af1d077cd3aadfde00c73f
naochael-jordan <naochael.jordan@gmail.com>
2018-12-09 07:37:43 +0900

    update

---
+++ ReactNative/rnf9/ios/Pods/Target Support Files/Pods-rnf9-tvOS/Pods-rnf9-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnf9_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_rnf9_tvOS
+@end
---
+++ ReactNative/rnf9/ios/Pods/Target Support Files/Pods-rnf9-tvOSTests/Pods-rnf9-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnf9_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_rnf9_tvOSTests
+@end
