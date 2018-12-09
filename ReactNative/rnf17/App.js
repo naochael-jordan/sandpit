3a39d93b5e3b4d4ee5bd91c569471ff4350d2e6b
naochael-jordan <naochael.jordan@gmail.com>
2018-12-09 15:53:07 +0900

    update

---
+++ ReactNative/rnf17/App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import firebase from "react-native-firebase";
+const main = async () => {
+  // 新しく追加 支出
+  await firebase
+    .firestore()
+    .collection("payments")
+    .add({
+      userId: "w0Wx7AaAVGlQOBS3RfYM",
+      value: 10000,
+      name: "日用品",
+      paymentAt: new Date("2019-7-15")
+    });
+};
+main();
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
---
+++ ReactNative/rnf17/ios/Pods/Target Support Files/Pods-rnf17/Pods-rnf17-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnf17 : NSObject
+@end
+@implementation PodsDummy_Pods_rnf17
+@end
